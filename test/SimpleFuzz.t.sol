2663fdb1356330618849d8d56cb667a5999672d9
wildanvin <danvin.jim@gmail.com>
2024-02-18 11:58:24 -0500

    Add invariant test with foundry aka stateful fuzzing

--- test/SimpleFuzz.t.sol
+++ test/SimpleFuzz.t.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {SimpleFuzz} from "../src/SimpleFuzz.sol";
 
-contract SimpleFuzzTest is Test {
+contract SimpleFuzzTest is StdInvariant, Test {
     SimpleFuzz public simpleFuzz;
 
     function setUp() public {
         simpleFuzz = new SimpleFuzz();
-        //counter.setNumber(0);
+        targetContract(address(simpleFuzz));
     }
 
     function testSimpleDoStuff() public {
@@ -19,6 +20,11 @@ contract SimpleFuzzTest is Test {
 
     function testFuzzDoStuff(uint256 x) public {
         simpleFuzz.doStuff(x);
+        assertEq(simpleFuzz.shouldAlwaysBeZero(), 0);
+    }
+
+    //This is to make invariant aka statefull fuzzing:
+    function invariant_testAlwaysReturnsZero () public {
         assertEq(simpleFuzz.shouldAlwaysBeZero(), 0);
     }
 }
